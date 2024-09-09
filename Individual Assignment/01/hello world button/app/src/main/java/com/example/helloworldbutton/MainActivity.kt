1f8ca754706e12eb9f662165ab5faafbc14574ff
739078545 <739078545@qq.com>
2024-09-08 22:35:16 -0400

    update

--- Individual Assignment/01/hello world button/app/src/main/java/com/example/helloworldbutton/MainActivity.kt
+++ Individual Assignment/01/hello world button/app/src/main/java/com/example/helloworldbutton/MainActivity.kt
@@ -5,14 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.helloworldbutton.ui.theme.HelloWorldButtonTheme
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
